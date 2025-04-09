0773562292162b4f124076637cd782088e94d355
Pravalika <112800726+Pravalika-27@users.noreply.github.com>
2025-04-09 20:21:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Hello im pravalika this is my Jenkins-CI/CD Task-2'));
+app.get('/', (req, res) => res.send('Hello im pravalika this is my Jenkins-CI/CD Task'));
 app.listen(3000, () => console.log('App running on port 3000'));
